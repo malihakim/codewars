7bbc881b18b924948e214fd29a181e4bb3218b5d
Maliha Hakim <72034231+malihakim@users.noreply.github.com>
2022-05-23 18:39:54 +0100

    Update create_phone_number.py

--- create_phone_number.py
+++ create_phone_number.py
@@ -1,3 +1,5 @@
+#Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
 def create_phone_number(n):
     areaCode = '(' + str(n[0]) + str(n[1]) + str(n[2]) + ') '
     nextThree = str(n[3]) + str(n[4]) + str(n[5]) + '-'
