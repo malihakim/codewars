ff67ce0529baab7f6877af0d76d8fb88e526054b
Maliha Hakim <72034231+malihakim@users.noreply.github.com>
2022-05-23 18:37:43 +0100

    Update pagination_helper.py

--- pagination_helper.py
+++ pagination_helper.py
@@ -1,3 +1,6 @@
+#For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+#The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page.
+
 class PaginationHelper:
     
     def __init__(self, collection, items_per_page):
