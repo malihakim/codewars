8ec1c038064df8009dcf4fdb6f62dc9ff5dacfc4
Maliha Hakim <72034231+malihakim@users.noreply.github.com>
2022-05-23 18:41:20 +0100

    Update humanReadableFormat.py

--- humanReadableFormat.py
+++ humanReadableFormat.py
@@ -1,3 +1,6 @@
+#Write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+#The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
 def format_duration(a):
     time = {'year':0, 'day':0, 'hour':0, 'minute':0, 'second':0} 
     
